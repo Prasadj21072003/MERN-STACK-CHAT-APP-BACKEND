2e15a7862ff829f71d8171e916749e0b27f89d02
Prasadj21072003 <prasadjadhav2003@gmail.com>
2024-09-16 23:47:46 +0530

    fourteen commit

--- dist/index.js
+++ dist/index.js
@@ -8,7 +8,7 @@ import { app, server } from "./socket/Socket.js";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 app.use(cors({
-    origin: "mern-stack-chat-app-frontend.vercel.app",
+    origin: "https://mern-stack-chat-app-frontend.vercel.app/",
     credentials: true,
 }));
 app.use(cookieParser());
